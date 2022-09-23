82e336eb39059220c26c9250db1df317ce47add7
dev.koo <35742284+koo6357@users.noreply.github.com>
2022-09-23 22:56:57 +0900

    feat: avatar 컴포넌트 추가

---
+++ apps/blog/src/components/avatar/index.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import Image from 'next/image'
+import styled from '@emotion/styled'
+import { css } from '@emotion/react'
+
+interface Props {
+  children?: React.ReactNode
+  className?: string
+  src: string | undefined
+  size?: number | 'large' | 'small',
+  shape?: 'circle' | 'square',
+}
+
+export default function Avatar ({ children, className, src = '', size = 32, shape = 'square' }: Props) {
+  const width = size === 'large'
+    ? 48
+    : size === 'small'
+      ? 24
+      : size
+  return (
+    <Container
+      className={className}
+      css={css`
+        width: ${width}px;
+        height: ${width}px;
+        border-radius: ${shape === 'circle' ? '50%' : '0'};
+        overflow: hidden;
+        border:1px solid #ddd;
+      `}
+    >
+      <Image
+        src={src}
+        layout={'fill'}
+      />
+      {children}
+    </Container>
+  )
+}
+
+const Container = styled.div`
+  position: relative;
+`
