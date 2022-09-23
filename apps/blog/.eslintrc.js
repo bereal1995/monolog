33172994f3e945299ae0f0f90882fbb7f31bf53e
dev.koo <35742284+koo6357@users.noreply.github.com>
2022-09-23 19:12:08 +0900

    fix: 수정

--- apps/blog/.eslintrc.js
+++ apps/blog/.eslintrc.js
@@ -10,9 +10,6 @@ module.exports = {
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaFeatures: {
-      jsx: true
-    },
     ecmaVersion: 'latest',
     sourceType: 'module'
   },
