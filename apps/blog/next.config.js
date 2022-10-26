dd00da86d4dff706af7424949e82b28197303356
bereal1995 <gygud98@gmail.com>
2022-10-27 01:23:53 +0900

    feat: 이미지 설정 수정

--- apps/blog/next.config.js
+++ apps/blog/next.config.js
@@ -5,5 +5,6 @@ module.exports = withTM({
   images: {
     // next/image 태그를 사용하기 위해선 domains에 이미지가 있는 도메인을 추가해야 한다.
     domains: ['avatars.githubusercontent.com', 's3.us-west-2.amazonaws.com'],
+    unoptimized: true,
   },
 })
