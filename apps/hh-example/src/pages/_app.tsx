3d331e988c4215504cfd7c609ed8ee70f71f6534
bereal1995 <gygud98@gmail.com>
2022-12-15 02:06:18 +0900

    test

--- apps/hh-example/src/pages/_app.tsx
+++ apps/hh-example/src/pages/_app.tsx
@@ -4,27 +4,42 @@ import { useState } from 'react'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { NextPageContext } from 'next'
 
+import { notification } from 'antd'
+
 import ScrollRemember from '../posts/infinite-scroll/components/ScrollRemember'
 
 import useUnhandledRejectionError from '../posts/error-boundary/hooks/useUnhandledRejectionError'
 
 import { captureUnhandledRejection } from '../posts/error-boundary/lib/sentry'
 
-import useAntdNotification from '../hooks/useAntdNotification'
-
 import GlobalStyle from '@/styles/GlobalStyle'
 
 type PageProps = {
   dehydratedState?: DehydratedState
 }
-
+type NotificationType = 'success' | 'info' | 'warning' | 'error'
 type ExtendedAppProps<P = {}> = {
   err?: NextPageContext['err']
 } & AppProps<P>
 
 function MyApp({ Component, pageProps }: ExtendedAppProps<PageProps>) {
   const [queryClient] = useState(() => new QueryClient())
-  const { contextHolder, openNotification } = useAntdNotification()
+  const [api, contextHolder] = notification.useNotification()
+
+  const openNotification = ({
+    type = 'error',
+    message,
+    description,
+  }: {
+    type?: NotificationType
+    message: string
+    description: string
+  }) => {
+    api[type]({
+      message,
+      description,
+    })
+  }
 
   useUnhandledRejectionError(({ reason: error }) => {
     captureUnhandledRejection(error)
